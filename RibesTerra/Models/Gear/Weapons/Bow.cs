301a6b92c971967982156f6379e1395090bf9885
unknown <eagan4eva@gmail.com>
2015-03-29 01:43:00 +0200

    Bow class added.

--- RibesTerra/Models/Gear/Weapons/Bow.cs
+++ RibesTerra/Models/Gear/Weapons/Bow.cs
@@ -6,5 +6,50 @@
 
     public class Bow : Gear, IGear, IWeapon
     {
+        private int attackPoints;
+        private int arrowAmount;
+
+        public Bow(string initialName, decimal initialPrice, string initialDescription, double initialWeight, int initialAttackPoints, int initialArrowAmount)
+            : base(initialName, initialPrice, initialDescription, initialWeight)
+        {
+
+        }
+
+        public int AttackPoints
+        {
+            get
+            {
+                return this.attackPoints;
+            }
+            protected set
+            {
+                if (value == 0 || value < 0)
+                {
+                    throw new ArgumentException("Attack points cannot be less or equal to zero!");
+                }
+                this.attackPoints = value;
+            }
+        }
+
+        public int ArrowAmount
+        {
+            get
+            {
+                return this.arrowAmount;
+            }
+            set
+            {
+                if (value == 0 || value < 0)
+                {
+                    throw new ArgumentException("Arrow amount cannot be less or equal to zero!");
+                }
+                if (value > 10)
+                {
+                    throw new ArgumentException("Arrow amount cannot be more than ten!");
+                }
+                this.arrowAmount = value;
+            }
+        }
+
     }
 }
