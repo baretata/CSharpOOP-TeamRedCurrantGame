77c37d52fb4d46d4a216ece0c62f91ca9c184522
baretata <zlatkoatanasov92@gmail.com>
2015-03-29 12:02:28 +0300

    Fixed interfaces

---
+++ RibesTerra/GameEngine/CreatureFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Models;
+using Models.Creatures;
+using Models.Creatures.Interfaces;
+
+namespace GameEngine
+{
+    public class CreatureFactory
+    {
+        public ICharacter CreateCharacter(string name, int power, int health, string gender, decimal gold)
+        {
+            return new Character(name, Character.InitialCharacterAttack, Character.InitialCharacterHealth,
+                GenderType.Male, Character.InitialCharacterGold);
+        }
+
+        public ICreature CreateEnemy(string name, int power, int health, string gender, decimal gold)
+        {
+            return new Character(name, Character.InitialCharacterAttack, Character.InitialCharacterHealth,
+                GenderType.Male, Character.InitialCharacterGold);
+        }
+    }
+}
