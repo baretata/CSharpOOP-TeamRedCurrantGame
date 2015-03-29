d395bc7862a1aa7c30059330b4a7a02e612ad7b0
unknown <eagan4eva@gmail.com>
2015-03-29 11:30:51 +0300

    Added class Sword.

--- RibesTerra/Models/Gear/Weapons/Sword.cs
+++ RibesTerra/Models/Gear/Weapons/Sword.cs
@@ -6,5 +6,28 @@
 
     public class Sword : Gear, IGear, IWeapon
     {
+        private int attackPoints;
+
+        public Sword(string initialName, decimal initialPrice, string initialDescription, double initialWeight, int initialAttackPoints)
+            : base(initialName, initialPrice, initialDescription, initialWeight)
+        {
+            this.AttackPoints = attackPoints;
+        }
+
+        public int AttackPoints
+        {
+            get
+            {
+                return this.attackPoints;
+            }
+            protected set
+            {
+                if (value == 0 || value < 0)
+                {
+                    throw new ArgumentException("Attack points cannot be less or equal to zero!");
+                }
+                this.attackPoints = value;
+            }
+        }
     }
 }
