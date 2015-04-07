235a2f1cb63bc35c0d81c05d2ebef85f01cf7307
baretata <zlatkoatanasov92@gmail.com>
2015-04-07 12:43:58 +0300

    Interfaces gathered in one folder

---
+++ RibesTerra/Models/Interfaces/ICreature.cs
@@ -0,0 +1,18 @@
+﻿namespace Models.Interfaces
+{
+    using System.Collections.Generic;
+
+    using Models.Creatures;
+    using Models.Interfaces;
+
+    public interface ICreature
+    {
+        int BaseHealth { get; set; }
+
+        int BasePower { get; set; }
+
+        GenderType Gender { get; }
+
+        HashSet<IGear> GearItems { get; }
+    }
+}
